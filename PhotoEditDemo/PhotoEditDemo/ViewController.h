74f3da3b9637ce0d991b3e0f57cf621e8aa8c957
周 <2192115519@qq.com>
2016-12-29 18:23:29 +0800

    创建图片编辑器
    图片编辑器

---
+++ PhotoEditDemo/PhotoEditDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoEditDemo
+//
+//  Created by 周文涛 on 2016/12/29.
+//  Copyright © 2016年 周文涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoEditDemo/PhotoEditDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoEditDemo
+//
+//  Created by 周文涛 on 2016/12/29.
+//  Copyright © 2016年 周文涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
